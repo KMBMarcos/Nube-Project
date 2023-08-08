6549aa531d8061933257c2d46745b03902c6f768
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-08-08 16:40:01 -0400

    Redirigiendo la URL raíz con la URL de home

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 
-urlpatterns = [
-
-]
+urlpatterns = []
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,14 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('home.urls')),
-    path('/', RedirectView.as_view(url='/home/', permanent=True)),
+    path('', RedirectView.as_view(url='/home/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 

02fd5fa2b31a1e1a03f2ac6da95fbef01f420256
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-08-13 23:51:47 -0400

    Registro de los modelos

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Dealer, Brand, Garment, GarmentInstance
 
 # Register your models here.
+
+# Instanciamos los modulos
+admin.site.register(Dealer)
+admin.site.register(Brand)
+admin.site.register(Garment)
+admin.site.register(GarmentInstance)

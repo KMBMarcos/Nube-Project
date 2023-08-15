dec3705eca8c8a159fe59434e86c979d232a3e8e
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-08-15 18:17:21 -0400

    Registrando los modelos admin de Garment, Brand y GarmentInstance

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,28 @@
+# Pasos para registrar los modulo
+
+
 from django.contrib import admin
 from .models import Department, Brand, Garment, GarmentInstance
 
+
 # Register your models here.
+
+
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+# Definicion del ModelAdmin de Garment
+@admin.register(Garment)
+class GarmentAdmin(admin.ModelAdmin):
+    pass
+
+# # Definicion de del ModelAdmin de GarmentInstance
+@admin.register(GarmentInstance) 
+class GarmentInstanceAdmin(admin.ModelAdmin):
+    pass 
 
 # Instanciamos los modulos
 admin.site.register(Department)
-admin.site.register(Brand)
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Garment)
 admin.site.register(GarmentInstance)

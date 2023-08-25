a22409b7f6821b23432f2837d573a63e6ff69ea0
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-08-24 21:54:13 -0400

    Creando la vista de index

--- home/views.py
+++ home/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
+from .models import Garment, Department, GarmentInstance
+
+
 # Create your views here.
+
+
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_garments = Garment.objects.all().count()
+    num_instances = GarmentInstance.objects.all().count()
+    # Prendas disponibles (status = 'a')
+    num_instances_available = GarmentInstance.objects.filter(status__exact="a").count()
+    num_departments = (
+        Department.objects.count()
+    )  # El 'all()' esta implícito por defecto.
+
+    # Renderiza la plantilla HTML base_generic.html con los datos en la variable contexto
+    return render(
+        request,
+        "base_generic.html",
+        context={
+            "num_garments": num_garments,
+            "num_instances": num_instances,
+            "num_instances_available": num_instances_available,
+            "num_departments": num_departments,
+        },
+    )

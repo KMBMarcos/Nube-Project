0d874eedf90a86aaf739324113a6261a59de484b
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-09-22 00:28:23 -0400

    Creacion de la vista de lista de department

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('garments/', views.GarmentListView.as_view(), name='garments'),
     path('garment/<int:pk>', views.GarmentDetailView.as_view(), name='garment_detail'),
+    path('departments/', views.DepartmentListView.as_view(), name='departments'),
+    path('department/<int:pk>', views.DepartmentDetailView.as_view(), name='department_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -32,7 +32,7 @@ def index(request):
 # Para ver la lista del modelo garment
 class GarmentListView(generic.ListView):
     model = Garment
-    
+    paginate_by = 10
     def get_context_data(self, **kwargs):
         # Llame primero a la implementación base para obtener un contexto.
         context = super(GarmentListView, self).get_context_data(**kwargs)
@@ -43,4 +43,20 @@ class GarmentListView(generic.ListView):
 # Para ver los detalles de los objetos del modelo garment
 class GarmentDetailView(generic.DetailView):
     model = Garment
-    garment_detail = 'garment_detail.html'
+    template_name = 'garment_detail.html'
+
+# Para la vista de la lista del modelo department
+class DepartmentListView(generic.ListView):
+    model = Department
+    paginate_by = 10
+    def get_context_data(self, **kwargs):
+        # Llame primero a la implementación base para obtener un contexto.
+        context = super(DepartmentListView, self).get_context_data(**kwargs)
+        # Obtenga el blog del id y agréguelo al contexto.
+        context['some_data'] = 'Estos son solo algunos datos'
+        return context
+
+# Para ver los detalles de los objetos del modelo department
+class DepartmentDetailView(generic.DetailView):
+    model = Department
+    template_name = 'deparment_detail.html'

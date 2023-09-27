afaea3c14022db8a6eba179df80e2499b5d2869f
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-09-26 23:27:36 -0400

    configuracion de la url para el login y creaciión del template

--- manage.py
+++ manage.py
@@ -1,13 +1,12 @@
-
 # Guia usada: https://developer.mozilla.org/es/docs/Learn/Server-side/Django/skeleton_website
 
 # ---> Django's command-line utility for administrative tasks. <---
 
 # Para la migracion de la base de datos: python manage.py migrate
 # Para correr el servidor: python manage.py runserver
-# Creción del usuario admin del sitio: python manage.py createsuperuser
+# Creción del supersuario del sitio: python manage.py createsuperuser
 
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 import os
 import sys
@@ -15,7 +14,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -27,5 +26,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

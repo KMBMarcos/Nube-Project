50c79ee374398e76b393a35ce3b3988f242d8c32
Marcos Mateo Tiburcio <106215917+KMBMarcos@users.noreply.github.com>
2023-11-26 13:48:39 -0400

    Actualizacion de dependencias

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 
 # ---> Django's command-line utility for administrative tasks. <---
 
+# Creacion de entorno virtual: python -m venv /path/to/new/virtual/environment
+# Activacion de entorno virtual: venv/Scripts/activate
 # Para la migracion de la base de datos: python manage.py migrate
 # Para correr el servidor: python manage.py runserver
 # Creción del supersuario del sitio: python manage.py createsuperuser
+# Creacion de dependencias: pip freeze > requirements.txt
+# Instalacion de dependencias: pip install -r requirements.txt
 
 # !/usr/bin/env python
 
